8c5333d15a2f1f0e964727c6c1d9cc23186a8b14
vain0 <id_vain@yahoo.co.jp>
2016-02-04 20:07:32 +0900

    [mtg] Add current files

---
+++ Mtg/MtgCore.fs
@@ -0,0 +1,438 @@
+﻿namespace Mtg
+
+open System
+open System.Text
+open Basis.Core
+open Util
+open Util.Collections
+
+// 2014/03/17 現在のルール
+
+[<AutoOpen>]
+module Core =
+  // 基本構造
+
+  type Natural = uint32
+  type NumOrVar =
+    | Num of Natural
+    | Var of string
+
+  // 色
+  type ColorAtom =
+    | White | Blue | Black | Red | Green
+  with
+    static member Cases =
+        Reflection.DU<ColorAtom>.UnitCases
+        
+    static member op_Explicit (this) =
+        match this with
+        | White -> 'W'
+        | Blue  -> 'U'
+        | Black -> 'B'
+        | Red   -> 'R'
+        | Green -> 'G'
+
+  type Color = Set<ColorAtom>
+  let colorless = new Color([])
+  let monocolor ca = new Color([ca])
+  let colorFromAtoms cas = new Color(cas)
+
+  let (|Colorless|MonoColored|MultiColored|) (clr : Color) =
+      match clr.Count with
+      | 0 -> Colorless
+      | 1 -> MonoColored (List.nth (clr |> Set.toList) 0)
+      | _ -> MultiColored clr
+
+  /// 「支払えるか否か」を基準として考える色。
+  /// つまり、混成と多色を区別する。命題論理式に似ている。
+  // TODO: 順番や重複を修正するプロパティ
+  type ColorPayable =
+    | Colorless
+    | Atom of ColorAtom
+    | Or  of ColorPayable list
+    | And of ColorPayable list
+  with
+    static member toColor (this : ColorPayable) = this.ToColor()
+    member this.ToColor() =
+        match this with
+        | Colorless -> colorless
+        | Atom ca -> monocolor ca
+        | Or cps | And cps
+            -> Set.unionMany (cps |> List.map ColorPayable.toColor)
+
+    (*
+    // 結合子
+    member private this.Combinator =
+        match this with
+        | Or _  -> Some ColorPayable.Or
+        | And _ -> Some ColorPayable.And
+        | _ -> None
+
+    // 標準形 (吸収律などを適用して冗長性を除去する、順番は適正化しない)
+    member this.NormalForm =
+        match this with
+        | Colorless | Atom _ -> this
+        | Or  cps -> (cps |> ColorPayable.NormalFormOfCps true  Or)
+        | And cps -> (cps |> ColorPayable.NormalFormOfCps false And)
+
+    static member private NormalFormOfCps isOr combinator cps =
+        match cps |> Seq.distinct |> List.ofSeq with
+        | [] -> Colorless
+        | [ cp ] -> cp.NormalForm
+        | cps ->
+            // Or [Or [...]; ...] のように入れ子になっているのは展開する
+            combinator (cps |> List.collect (fun cp ->
+                match cp.NormalForm with
+                | Or  cpsInner when     isOr -> cpsInner
+                | And cpsInner when not isOr -> cpsInner
+                | cp -> [ cp ]
+            ))
+                
+    // 選言標準形
+    member this.DisjNormalForm =
+        this.NormalForm.DisjNormalForm'
+    member private this.DisjNormalForm' =
+        match this with
+        | Colorless | Atom _ -> this //.NormalForm
+        | Or cps -> Or (cps |> List.map (fun cp -> cp.DisjNormalForm))
+        | And cps -> ColorPayable.DNF_AndCps cps
+
+    // 分配律を使って And を内側に送り込む
+    static member private DNF_AndCps cps =
+        cps
+        |> List.map (fun cp -> cp.DisjNormalForm)
+        |> List.map (function
+            | Or cpsOr -> cpsOr
+            | cp -> [cp] )
+        |> List.product
+        |> List.map And
+        |> Or
+    //*)
+
+  // タイプ
+  type Supertype =
+    | Basic
+    | Legendary
+    | Snow
+    | World
+  with
+    static member Cases =
+        Reflection.DU<Supertype>.UnitCases
+
+    static member Names =
+        Reflection.DU<Supertype>.Names
+
+  type CardType =
+    | Artifact
+    | Creature
+    | Enchantment
+    | Land
+    | Planeswalker
+    | Instant
+    | Sorcery
+    | Tribal
+  with
+    static member Cases =
+        Reflection.DU<CardType>.UnitCases
+        
+    static member Names =
+        Reflection.DU<CardType>.Names
+
+    static member op_Explicit (this) =
+        match this with
+        | Artifact     -> 'A'
+        | Creature     -> 'C'
+        | Enchantment  -> 'E'
+        | Land         -> 'L'
+        | Planeswalker -> 'P'
+        | Instant      -> 'I'
+        | Sorcery      -> 'S'
+        | Tribal       -> 'T'
+
+  // シンボル
+  type ManaSymbol =
+    /// {0}, {1}, ..., {15}, ...
+    | Unspecified of Natural
+    /// {X}, {Y}, {Z}
+    | Var of char
+    /// {W}, {U}, {B}, {R}, {G}
+    | Monocolored of ColorAtom
+    /// {W/U}, {2/B}, {R/P}, ...
+    | Hybrid of ManaSymbol * ManaSymbol
+    /// {C}
+    | Colorless
+    /// {S}
+    | Snow
+    /// Represents "2 life".
+    /// This's used as half of hybrid mana symbol to represent a Phyrexian mana symbol.
+    | TwoLife
+    /// {M/2}, such as {W/2} (ex. 《Little Girl》)
+    //| Half of ManaSymbol
+    /// {∞} (《Mos Lotus》)
+    //| Infinite
+  with
+    static member (/) (lhs, rhs) =
+        Hybrid (lhs, rhs)
+
+    static member PhyrexianManaSymbol clra =
+        (Monocolored clra) / TwoLife
+
+    member this.ColorPayable =
+        match this with
+        | Monocolored ca ->
+            ColorPayable.Atom ca
+        | Hybrid (lhs, rhs) ->
+            ColorPayable.Or [lhs.ColorPayable; rhs.ColorPayable]
+        | Unspecified _
+        | Var _
+        | Colorless
+        | Snow
+        | TwoLife ->
+            ColorPayable.Colorless
+    member this.Color =
+        this.ColorPayable.ToColor()
+
+  type ManaCost = ManaSymbol list //Bag<ManaSymbol>
+  let colorFromManaCost (mc : ManaCost) =
+      mc |> List.fold (fun clr sym -> clr + sym.Color) colorless
+
+  type PhaseSymbol =
+    | TapSymbol
+    | UntapSymbol
+  with
+    static member Cases =
+        Reflection.DU<PhaseSymbol>.UnitCases
+
+  type LoyaltySymbol =
+    | LoyaltySymbol of (NumOrVar * (int -> int))
+
+// MTG の静的表現、つまりゲーム外の状態を表現する型
+[<AutoOpen>]
+module Static =
+  open Core
+
+  type Expansion = {
+      FullName : string
+      ShortName : string  // 3 characters
+    }
+
+  type Rarity =
+    | Common | Uncommon | Rare | MythicRare
+  with
+    static member Cases =
+        Reflection.DU<Rarity>.UnitCases
+
+  type PowTouLoyalty =
+      | PowTou of (int * int)
+      | Loyalty of int
+
+  type FlavorText =
+      string
+
+  type Spec = {
+      Name : string
+      ManaCost : ManaSymbol list
+      Color : Color
+      ColorIdent : Color option
+      CardType : Set<CardType>
+      Supertype : Set<Supertype>
+      Subtype : Set<string>
+          // Subtype の型は正確には「Set<ArtifactType> * Set<CreatureType> * ...」だが、面倒くさい。
+      RuleText : string
+      PowTou : (int * int) option
+      Loyalty : int option
+    }
+  with
+    static member ofMinimal (name, ruleText) =
+        { Name          = name
+          ManaCost      = []
+          Color         = colorless
+          ColorIdent    = None
+          Supertype     = Set.empty
+          CardType      = Set.empty
+          Subtype       = Set.empty
+          RuleText      = ruleText
+          PowTou        = None
+          Loyalty       = None
+        }
+    static member ofCreature name manaCost subtypes ruleText powTou =
+        { Spec.ofMinimal (name, ruleText) with
+            ManaCost    = manaCost
+            Color       = colorFromManaCost manaCost
+            CardType    = Set.singleton Creature
+            Subtype     = Set.ofSeq subtypes
+            PowTou      = Some powTou
+        }
+    static member ofPlaneswalker name manaCost subtype ruleText loyalty =
+        { Spec.ofMinimal (name, ruleText) with
+            ManaCost    = manaCost
+            Color       = colorFromManaCost manaCost
+            CardType    = Set.singleton Planeswalker
+            Subtype     = Set.singleton subtype
+            Loyalty     = Some loyalty
+        }
+
+  type SplitCard = Spec list
+
+  /// represents a "physical" card
+  type Card =
+    | RegularCard     of Spec
+    | FlipCard        of Spec * Spec
+
+    /// FrontFace, BackFace
+    | DoubleFacedCard of Spec * Spec
+        
+    /// LeftHalf, RightHalf
+    | SplitCard       of SplitCard
+
+  type SingleCardSpec = {
+      Card : Card
+      Expansions : Map<Expansion, (Rarity * FlavorText)>
+    }
+  with
+    static member ofCard (card) =
+      { Card = card; Expansions = Map.empty }
+
+// 動的表現、つまりゲーム中のものを表現する型
+// 使い道なし
+(*
+module Dynamic =
+  open Core
+
+  /// "sacrifice a creature", "tap an artifact you control", or something
+  type Action =
+      string
+
+  // 能力
+  type Cost =
+      ManaCost * Set<PhaseSymbol> * (Action list)
+
+  type Cond = 
+    | TriggerAt of string // BeginningOfUpkeep
+    | TriggerWhen of string
+
+  type Ability =
+    | Activated of Cost * string
+    | Triggered of Cond * string
+    | Static of string
+    | Spell of string
+
+  // カード
+  type SplitCard = CardSpec * CardSpec
+
+  /// represents a "physical" card
+  and Card =
+    | RegularCard     of CardSpec
+    | FlipCard        of CardSpec * CardSpec
+        
+    /// FrontFace, BackFace
+    | DoubleFacedCard of CardSpec * CardSpec
+        
+    /// LeftHalf, RightHalf
+    | SplitCard       of SplitCard
+
+  and Spec =
+    {
+      Name : string
+      ManaCost : unit//Bag<Mana>
+      Color : Color
+      ColorIdentity : Color
+      CardType : Set<CardType>
+      Supertype : Set<Supertype>
+      Subtype : Map<Supertype, Set<string>>
+          // Subtype の型は正確には「Set<ArtifactType> * Set<CreatureType> * ...」だが、
+          // それを型として記述するのは難しい。
+      RuleText : string
+      Ability : unit // Bag<Ability>
+      Power : int
+      Toughness : int
+      Loyalty : int
+    }
+
+  and CardSpec = {
+      Spec : Spec
+      Flavor : string
+      ExpansionSymbol : string * string
+    }
+
+  type AbilObj = {
+      Source : Object
+      Original : Ability
+    }
+
+  /// represents an object (in MTG rule)
+  and Object =
+    | CardObj of Card
+    | Token of CardSpec
+    | Copy of Object
+    | AbilObj of AbilObj
+    | FusedSplitSpell of SplitCard
+    
+  type ObjectInstance = {
+      Position : string//Area
+      Object : Object
+    }
+  ()
+//*)
+
+/// string expression
+[<AutoOpen>]
+module StrExp =
+  /// language tag
+  /// Not used
+  type Lang =
+    /// en_US (default)
+    | En
+    /// ja_JP
+    | Ja
+
+  type ColorAtom with
+      static member Chars =
+          ColorAtom.Cases |> List.map char
+
+      static member FromChar(c) =
+          ColorAtom.Cases
+          |> List.tryFind (fun ca -> (char ca) = c)
+
+  type CardType with
+      static member FromChar(c) =
+          CardType.Cases
+          |> List.tryFind (fun ct -> (char ct) = c)
+
+  [<AutoOpen>]
+  module Ja =
+    type ColorAtom with
+        static member JaChars =
+            [ '白'; '青'; '黒'; '赤'; '緑' ]
+
+        static member FromJaChar (jaChar) =
+            (ColorAtom.JaChars, ColorAtom.Cases) |> List.tryAssocUnzip jaChar
+
+    type Supertype with
+        static member ToJaName = function
+          | Basic           -> "基本"
+          | Legendary       -> "伝説の"
+          | Supertype.Snow  -> "氷雪"
+          | World           -> "ワールド"
+        static member JaNames =
+            Supertype.Cases |> List.map Supertype.ToJaName
+
+        static member FromJaName (jaName) =
+            Supertype.Cases |> List.tryFind (fun st -> Supertype.ToJaName st = jaName)
+
+    type CardType with
+        static member ToJaName = function
+          | Artifact        -> "アーティファクト"
+          | Creature        -> "クリーチャー"
+          | Enchantment     -> "エンチャント"
+          | Land            -> "土地"
+          | Planeswalker    -> "プレインズウォーカー"
+          | Instant         -> "インスタント"
+          | Sorcery         -> "ソーサリー"
+          | Tribal          -> "部族"
+
+        static member JaNames =
+            CardType.Cases |> List.map (CardType.ToJaName)
+
+        static member FromJaName (jaName) =
+            (CardType.JaNames, CardType.Cases) |> List.tryAssocUnzip jaName
---
+++ Mtg/MtgHandmade.fs
@@ -0,0 +1,451 @@
+﻿namespace Mtg
+
+open System
+open System.Text
+open FParsec
+open Basis.Core
+open Util
+open Util.Collections
+
+module Handmade =
+  type CardName =
+    {
+      En: string option
+      Ja: string option
+    }
+  with
+    static member Empty =
+        { Ja = None; En = None }
+
+    override this.ToString() =
+        match (this.En, this.Ja) with
+        | Some en, Some ja -> en + "/" + ja
+        | enOpt, jaOpt ->
+            (enOpt |> Option.getOr "") + (jaOpt |> Option.getOr "")
+
+  let colorFromIdentityOrManaCost ciOpt manacost =
+      ciOpt |> Option.getOr' (lazy colorFromManaCost manacost)
+
+  [<AutoOpen>]
+  module private Detail =
+    type Parser<'T> = Parser<'T, unit>
+
+    module ErrorMessageList =
+      let rec toList (self: ErrorMessageList) =
+          if self = null
+          then []
+          else (self.Head :: toList self.Tail)
+
+    let skip p =
+        p |>> ignore
+
+    /// 改行ではない空白類
+    let blankMany: Parser<_> =
+        manyChars (anyOf " \t　")
+
+    let blankMany1 =
+        notEmpty blankMany
+
+    let skipEmptyLine =
+        skip (blankMany .>> skipNewline)
+
+    /// 改行を除く任意の文字列
+    let anyInLine: Parser<_> =
+        manyChars (noneOf "\r\n")
+
+    let skipDotOrBlank =
+        skipChar '・' <|> skip blankMany1
+
+    let betweenParen l r =
+        between (skipChar l) (skipChar r)
+
+    /// Apply `choice ps`.
+    /// If ps.[i] has succeeded and resulted in `x` then return ``fs.[i] x``.
+    let lookupUnzip ps fs =
+        assert ((ps |> Seq.length) = (fs |> Seq.length))
+        (ps, fs)
+        ||> Seq.map2 (|>>)
+        |> choice
+
+    let lookupStrMap strs vals =
+        lookupUnzip (strs |> List.map skipString) (vals |> List.map konst)
+
+    let inline choiceEnum<'T> =
+        lookupStrMap
+          Reflection.DU<'T>.Names
+          Reflection.DU<'T>.UnitCases
+          
+    /// Same as ``sepBy p sep`` except for this never fails
+    let safeSepBy p sep =
+        opt (attempt (pipe2 p (many (attempt (sep >>. p))) List.cons))
+        |>> Option.getOr []
+
+    ///Unwrap each element in tuple of options if able
+    let unwrapOption2 = function
+      | (Some v0, Some v1) -> Some (v0, v1)
+      | _ -> None
+
+  // 文法定義
+  module CardSyntaxParser =
+    let colorAtomCharFrom (chars) =
+        let ps =
+          (ColorAtom.Cases, chars)
+          ||> List.map2 (fun ca c -> skipChar c >>% ca)
+        choice ps
+
+    let colorAtomChar: Parser<_> =
+        (   colorAtomCharFrom (ColorAtom.Chars)
+        <|> colorAtomCharFrom (ColorAtom.JaChars)
+        )
+
+    let manaSymbol =
+        let minimal =
+            (   (puint32          |>> ManaSymbol.Unspecified)
+            <|> (colorAtomChar    |>> ManaSymbol.Monocolored)
+            <|> (anyOf "C◇"      >>% ManaSymbol.Colorless)
+            <|> (anyOf "PΦ"      >>% ManaSymbol.TwoLife)
+            <|> (anyOf "S氷"      >>% ManaSymbol.Snow)
+            <|> (anyOf "XYZ"      |>> ManaSymbol.Var)
+            )
+
+        let hybridMany1 =
+            pipe2
+              minimal 
+              (many (skipChar '/' >>. minimal))
+              (List.fold (curry2 ManaSymbol.Hybrid))
+
+        let symbol = hybridMany1
+
+        ( attempt
+            (betweenParen '{' '}' symbol)
+        <|> (betweenParen '(' ')' symbol)
+        )
+
+    let manaCost: Parser<_> =
+        many manaSymbol
+
+    let colorIdent =
+        betweenParen '[' ']'
+          (sepBy colorAtomChar (skipChar '/'))
+
+    let typeLineTmpl supertypeList cardtypeList subtypeList =
+        let separator =
+            skipMany1 (skipAnyOf  "-－―~～") >>. blankMany
+
+        tuple4
+          (opt colorIdent
+            .>> blankMany)
+          (supertypeList
+            .>> optional skipDotOrBlank)
+          (cardtypeList
+            .>> blankMany)
+          (opt
+            (separator
+            >>. subtypeList
+            //TODO: rarity
+            ) |>> Option.getOr [])
+         .>> blankMany .>> skipNewline
+
+    // '*' を含む式
+    let starExpr =
+        regex @"[0-9XYZ+() \t-]*\*[0-9XYZ+*() \t-]*"
+
+    let intOrStarExpr =
+        (   (attempt pint32  |>> Some)
+        <|> (starExpr        >>% None)
+        )
+
+    /// P/T
+    /// '*' の式は、認識するが理解しない
+    let powTou =
+        tuple2
+          (intOrStarExpr .>> skipChar '/') intOrStarExpr
+        |>> (unwrapOption2 >> Option.map PowTou)
+
+    let loyalty =
+        skipString "loyalty:" >>. blankMany
+        >>. intOrStarExpr
+        |>> Option.map Loyalty
+
+    let powTouLoyalty =
+        attempt powTou
+        <|> loyalty
+
+    /// コメント行
+    /// 行頭でだけ使う
+    let commentLine =
+        skipChar '#' >>. restOfLine false
+
+    let commentLines =
+        sepBy commentLine skipNewline
+
+    let skipNewlineAndCommentLines =
+        skipNewline >>. skip commentLines
+
+    let flavorTextKeyword =
+        skipString "FT" >>. skipAnyOf ":："
+
+    /// フレイバーテキスト
+    /// 行頭でだけ使う
+    let flavorTextLines =
+        let forbidden =
+            attempt skipEmptyLine
+
+        flavorTextKeyword >>. blankMany
+        >>. sepBy
+              (notFollowedBy forbidden >>. restOfLine false)
+              skipNewlineAndCommentLines
+        |>> Str.join "\r\n"
+
+    let ruleTextLines =
+        let forbidden =
+            attempt (blankMany >>. skip powTouLoyalty)
+            <|> attempt flavorTextKeyword
+            <|> skipEmptyLine
+
+        let line =
+            notFollowedBy forbidden
+            >>. restOfLine false
+
+        commentLines
+        >>. sepBy line skipNewlineAndCommentLines
+        |>> Str.join "\r\n"
+
+    [<AutoOpen>]
+    module En =
+      let supertypeList =
+          let p = lookupStrMap Supertype.Names Supertype.Cases
+          safeSepBy p blankMany1 
+
+      let cardTypeList =
+          safeSepBy choiceEnum<CardType> blankMany1
+
+      let subtype =
+          many1Chars (letter <|> digit <|> anyOf "'-_")
+
+      let subtypeList =
+          sepBy subtype blankMany1
+
+      let typeLine =
+          typeLineTmpl supertypeList cardTypeList subtypeList
+
+    module Ja =
+      /// 日本語/英語
+      let cardName =
+          let enName =
+              manyChars (noneOf "/》")
+              |>> Option.ifSat (not << Str.isNullOrEmpty)
+
+          let jaName =
+              opt (
+                skipChar '/'
+                >>. manyChars (noneOf "》")
+                )
+
+          betweenParen '《' '》' 
+            (pipe2 enName jaName (fun en ja -> { En = en; Ja =  ja }))
+
+      let supertypeList =
+          safeSepBy
+            (lookupStrMap Supertype.JaNames Supertype.Cases)
+            (optional skipDotOrBlank)
+
+      let cardTypeList =
+          safeSepBy
+            (lookupStrMap CardType.JaNames CardType.Cases)
+            (optional skipDotOrBlank)
+
+      /// 英名併記を許可
+      let subtype =
+          many1Chars letter
+          .>> optional (betweenParen '(' ')' En.subtype)
+
+      let subtypeList =
+          sepBy subtype skipDotOrBlank
+
+      let typeLine =
+          typeLineTmpl supertypeList cardTypeList subtypeList
+
+    /// カード
+    let cardSpec: Parser<_> = parse {
+        let nl = skipNewlineAndCommentLines
+        let typeLineParser =
+            attempt En.typeLine <|> Ja.typeLine
+
+        let! name     = Ja.cardName      .>> blankMany .>> optional (attempt nl)
+        let! manaCost = manaCost         .>> blankMany .>> nl
+        let! typeLine = typeLineParser
+        let! ruleText = ruleTextLines    .>> blankMany
+        let! ptl      = powTouLoyalty
+        do! if ptl.IsSome then blankMany >>. nl else preturn ()
+
+        let (colorIdent, supertypes, cardtypes, subtypes) = typeLine
+        let colorIdent = colorIdent |> Option.map colorFromAtoms
+        return {
+            Name       = string name
+            ManaCost   = manaCost
+            Color      = colorFromIdentityOrManaCost colorIdent manaCost
+            ColorIdent = colorIdent
+            Supertype  = Set.ofList supertypes
+            CardType   = Set.ofList cardtypes
+            Subtype    = Set.ofList subtypes
+            RuleText   = ruleText
+            PowTou     = (match ptl with | Some (PowTou pt) -> Some pt | _ -> None)
+            Loyalty    = (match ptl with | Some (Loyalty l) -> Some l  | _ -> None)
+          }
+      }
+
+    let regularCard: Parser<_> =
+        cardSpec |>> RegularCard
+
+    let flipOrDoubleFacedCard: Parser<_> =
+        let connector =
+                (stringReturn "<flip>" FlipCard)
+            <|> (stringReturn "<double-faced>" DoubleFacedCard)
+        pipe3
+          cardSpec connector cardSpec
+          (fun c1 ctor c2 -> ctor (c1, c2))
+
+    let splitCard: Parser<_> =
+        sepBy1 cardSpec (skipString "<split>")
+        |>> SplitCard
+
+    let cardBody =
+        (attempt regularCard)
+        <|> (attempt flipOrDoubleFacedCard)
+        <|> splitCard
+
+    let cardSingle = parse {
+        let! card = cardBody
+        let! ft   = opt flavorTextLines
+        // TODO: analyze comments and build expansions
+        return SingleCardSpec.ofCard(card)
+      }
+
+    let skipBlankLines1 =
+        skipMany1
+          (skipNewline
+            >>. (skip commentLines <|> (blankMany >>. followedByNewline)))
+
+    let cardList =
+        sepBy cardSingle skipBlankLines1
+
+    let cardListFile =
+        blankMany
+        >>. optional skipBlankLines1
+        >>. cardList
+        .>> optional skipBlankLines1
+        .>> eof
+
+    let test () =
+        let allSuccess p cases =
+            cases
+            |> List.iter (fun (expr, xp) ->
+              let result = run p expr
+              match result with
+              | ParserResult.Success (value, _, _) ->
+                  if xp <> value
+                  then failwithf "WA\r\nexpected = %A\r\nbut result is %A" xp value
+              | ParserResult.Failure (message, _, _) ->
+                  failwithf "Failed parsing:\r\ninput = %s\r\n%s" expr message
+              )
+
+        do
+            let (w, u, b, r, g) =
+                ( ManaSymbol.Monocolored White
+                , ManaSymbol.Monocolored Blue
+                , ManaSymbol.Monocolored Black
+                , ManaSymbol.Monocolored Red
+                , ManaSymbol.Monocolored Green
+                )
+            let p = ManaSymbol.TwoLife
+            let num = ManaSymbol.Unspecified
+            [ ""                    , []
+              "{1}"                 , [num 1u]
+              "{15}"                , [num 15u]
+              "{W}{U}{B}{R}{G}"     , [w; u; b; r; g]
+              "(白)(青)(黒)(赤)(緑)", [w; u; b; r; g]
+              "{W}{W}{U}"           , [w; w; u]
+              "{X}"                 , [ManaSymbol.Var 'X']
+              "{C}"                 , [ManaSymbol.Colorless]
+              "{S}"                 , [ManaSymbol.Snow]
+              "{U/P}"               , [u/p]
+              "{W/U}"               , [w/u]
+              "{2/W}"               , [(num 2u)/w]
+              "{2/W/U}"             , [(num 2u)/w/u]
+            ] |> allSuccess manaCost
+
+        do
+            [ "[]"       , []
+              "[W]"      , [White]
+              "[W/U]"    , [White; Blue]
+              "[W/W/U]"  , [White; White; Blue]
+            ] |> allSuccess colorIdent
+
+        do
+            [ "Legendary Creature"      , [Legendary]
+              "World Basic Land"        , [World; Basic]
+            ] |> allSuccess (En.supertypeList)
+
+            [ "伝説の"                  , [Legendary]
+              "ワールド・基本土地"      , [World; Basic]
+              "伝説の氷雪・部族・土地"  , [Legendary; Supertype.Snow]
+            ] |> allSuccess (Ja.supertypeList)
+
+            [ "Creature"                , [Creature]
+              "Artifact Land"           , [Artifact; Land]
+            ] |> allSuccess (En.cardTypeList)
+
+            [ "クリーチャー"            , [Creature]
+              "土地・エンチャント"      , [Land; Enchantment]
+              "部族アーティファクト"    , [Tribal; Artifact]
+            ] |> allSuccess (Ja.cardTypeList)
+
+            [ "インスタント"
+                , (None, [], [Instant], [])
+              "プレインズウォーカー ― ティボルト(Tibolt)"
+                , (None, [], [Planeswalker], ["ティボルト"])
+              "伝説の氷雪クリーチャーエンチャント ― 人間・戦士"
+                , ( None, [Legendary; Supertype.Snow]
+                  , [Creature; Enchantment], ["人間"; "戦士"])
+              "[白/青] アーティファクト ― 装備品(Equipment)"
+                , (Some[White; Blue], [], [Artifact], ["装備品"])
+            ]
+            |> List.map (fun (expr, r) -> (expr + "\r\n", r)) 
+            |> allSuccess (Ja.typeLine)
+            
+            [ "Instant"
+                , (None, [], [Instant], [])
+              "Planeswalker -- Tibolt"
+                , (None, [], [Planeswalker], ["Tibolt"])
+              "Legendary Snow Enchantment Creature -- Human Warrior"
+                , ( None, [Legendary; Supertype.Snow]
+                  , [Enchantment; Creature], ["Human"; "Warrior"])
+              "[W/U] Artifact~Equipment"
+                , (Some[White; Blue], [], [Artifact], ["Equipment"])
+            ]
+            |> List.map (fun (expr, r) -> (expr + "\r\n", r))
+            |> allSuccess (En.typeLine)
+
+            [ """《灰色熊/Grizzly Bear》 (1)(緑)
+                  クリーチャー ― 熊(Bear)
+                  2/2
+              """
+              , let mc = [ManaSymbol.Unspecified 1u; ManaSymbol.Monocolored Green] in
+                  RegularCard (Spec.ofCreature "灰色熊/Grizzly Bear" mc ["熊"] "" (2, 2))
+            ] |> allSuccess (cardBody)
+        ()
+
+  open System.IO
+  let main () =
+      CardSyntaxParser.test ()
+      let text = File.ReadAllText (@"D:\Docs\archive\_nobak\__testdata.txt")
+      let result =
+          let p = CardSyntaxParser.cardListFile
+          run p text
+      match result with
+      | ParserResult.Success (value, stat, pos) ->
+          ()
+      | ParserResult.Failure (message, err, stat) ->
+          ()
+      ()

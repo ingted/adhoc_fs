10879f2039857cfede0821b8148aa118a2ddbf4b
vain0 <id_vain@yahoo.co.jp>
2016-03-31 16:28:38 +0900

    WIP

--- AdhocFs/Program.fs
+++ AdhocFs/Program.fs
@@ -1,7 +1,40 @@
 ﻿open System
+open FsYaml
+
+//(*
+type Node<'T> =
+  {
+    Value   : 'T
+    Node    : list<Node<'T>>
+  }
+(* )
+
+type Node =
+  {
+    Value : int
+    Node : list<Node>
+  }
+  //*)
 
 [<EntryPoint>]
 let main argv =
+
+  let node =
+    {
+      Value = 1
+      Node =
+        [
+          {
+            Value = 2
+            Node = []
+          }
+        ]
+    }
+
+  let s = Yaml.dump(node)
+  printfn "%s" s
+
+  let n = Yaml.tryLoad<Node<int>> s
 
   // exit code
   0

ffefa71fefbd85b9c9bf0ee783ba665387581f89
vain0 <id_vain@yahoo.co.jp>
2016-04-29 18:25:41 +0900

    doesnt work

--- AdhocFs/Program.fs
+++ AdhocFs/Program.fs
@@ -1,11 +1,19 @@
 namespace AdhocFs
 
 open System
+open FSharp.Data
 
 module Program =
+  [<Literal>]
+  let connectionString = @"Server=localhost;Database=test;Uid=root;Pwd=;"
+
   [<EntryPoint>]
-  let main argv = 
-    printfn "%A" argv
+  let main argv =
+    use cmd =
+      new SqlCommandProvider<"
+        select * from users", @"Server=localhost;Database=test;Uid=root;Pwd=;">()
+
+
 
     // exit code
     0

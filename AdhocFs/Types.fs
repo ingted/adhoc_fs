0a318b658b29790b559fa805aa33d8185c9754d7
vain0 <id_vain@yahoo.co.jp>
2016-05-05 20:27:54 +0900

    Use accessors

--- AdhocFs/Types.fs
+++ AdhocFs/Types.fs
@@ -4,23 +4,12 @@ open System.ComponentModel.DataAnnotations
 
 [<AutoOpen>]
 module Types =
-  // レコードにはできないようだ
   [<AllowNullLiteral>]
-  type User(name': string) =
+  type User(name: string) =
     let mutable id = 0
-    let mutable name = name'
-    let mutable profile = (null: string)
 
     new () = User(null)
 
-    member this.Id
-      with get () = id
-      and  set v  = id <- v
-
-    member this.Name
-      with get () = name
-      and  set v  = name <- v
-
-    member this.Profile
-      with get () = profile
-      and  set v  = profile <- v
+    member val Id           = 0                 with get, set
+    member val Name         = name              with get, set
+    member val Profile      = (null: string)    with get, set
